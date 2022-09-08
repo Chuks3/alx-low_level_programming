19ecf228309c37b1670d9f6f95fad7fc301889fd
Chuks <chuksonyekams@gmail.com>
2022-09-08 15:37:06 -0700

    C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
+/**
 * A program that prints
 * Programming is like building a multilingual puzzle
 * with puts instead of printf
 */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle\n");
+  puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }

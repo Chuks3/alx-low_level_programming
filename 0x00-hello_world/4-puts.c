4c4c5a920cc4db1118df5b341006f811b2f026ea
Chuks <chuksonyekams@gmail.com>
2022-09-08 16:23:36 -0700

    C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* main - prints the 
+* main - prints a space.
 * Return: 0
 */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }

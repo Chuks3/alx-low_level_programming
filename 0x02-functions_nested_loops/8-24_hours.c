4bbf5896b11a25dd66226ef950d6d7bedaa1d89f
Chuks <chuksonyekams@gmail.com>
2022-09-13 20:36:14 -0700

    C programming

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,25 +1,24 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute.
- * Return: Always 0.
+ * jack_bauer - Entry point.
  */
 
 void jack_bauer(void)
 {
-int i;
-int j;
+	int i;
+	int j;
 
-for (i = 0; i < 23; i++)
-{
-for (j = 0; j < 59; j++)
-{
-_putchar((i / 10) + '0');
-_putchar((i % 10) + '0');
-_putchar(':');
-_putchar((j / 10) + '0');
-_putchar((j % 10) + '0');
-}
-_putchar('\n');
-}
+	for (i = 0; i <= 23; i++)
+	{
+		for (j = 0; j <= 59; j++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
+		}
+	}
 }

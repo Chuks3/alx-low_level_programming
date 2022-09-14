9a080df46a4fbeee20500b9aeb21bb6872410012
Chuks <chuksonyekams@gmail.com>
2022-09-13 20:19:25 -0700

    C programming

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int counter = 2;
+
+	float x = 1;
+	float y = a + 1;
+	float z = a + b;
+
+	printf("%.0f, ", x);
+	printf("%.0f, ", y);
+	while (counter < 98)
+	{
+		counter++;
+		printf("%.0f", z);
+		x = y;
+		y = z;
+		z = x + y;
+		if (counter < 98)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+	return (0);
+}

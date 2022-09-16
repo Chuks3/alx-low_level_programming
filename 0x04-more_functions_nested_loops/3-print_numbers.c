edb388f3228a6015d8a1fea18b97555ecebf4852
Chuks <chuksonyekams@gmail.com>
2022-09-16 16:45:10 -0700

    Debugging

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,8 +2,6 @@
 
 /**
  * print_numbers - prints numbers.
- * @a: Interger to be printed.
- * Return: Always 0.
  */
 
 void print_numbers(void)
@@ -15,5 +13,4 @@ for (a = 0; a < 10; a++)
 _putchar(a + '0');
 }
 _putchar('\n');
-return (0);
 }

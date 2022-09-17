4087921bedd2ce42a5f866626338d7c83b5beba9
Chuks <chuksonyekams@gmail.com>
2022-09-16 18:53:58 -0700

    Debugging

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,9 +1,17 @@
 #include "main.h"
 
 /**
- *
+ * print_line - prints a line.
+ * @n: An interger.
  */
 
 void print_line(int n)
 {
-  
+int a;
+
+for (a = 0; a < n; a++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}

2f3db8a0c8a95b303c24910ca78a44fec212bd34
Chuks <chuksonyekams@gmail.com>
2022-09-16 17:24:43 -0700

    Debugging

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints more numbers.
+ */
+
+void more_numbers(void)
+{
+int a;
+
+for (a = 1; a <= 10; a++)
+{
+int b;
+
+for (b = 0; b < 15; b++)
+{
+_putchar(b + '0');
+}
+_putchar('\n');
+}
+}

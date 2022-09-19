48a9f6ea7ca3271016f083c696c3f6e70f03d9c0
Chuks <chuksonyekams@gmail.com>
2022-09-19 10:06:31 -0700

    Debugging

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,11 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef main.h
+#define main.h
 
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void rev_string(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void puts2(char *str);

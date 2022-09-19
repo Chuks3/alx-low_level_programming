11aa5a203a92c3d8180178ae12c17b2fa53c7ea6
Chuks <chuksonyekams@gmail.com>
2022-09-19 07:28:07 -0700

    Debugging

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string.
+ * @str: character to be printed.
+ **/
+
+void _puts(char *str)
+{
+str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+_puts(str);
+}

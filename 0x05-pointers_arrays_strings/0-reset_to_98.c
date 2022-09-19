bb0cdedc9f75aac6bca89ca300112d40b35645f0
Chuks <chuksonyekams@gmail.com>
2022-09-19 05:21:40 -0700

    Debugging

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets a value to 98.
+ * @n: The corresponding interger.
+ **/
+
+void reset_to_98(int *n)
+{
+int a = 5;
+
+n = &a;
+*n = 98;
+return (0);
+}

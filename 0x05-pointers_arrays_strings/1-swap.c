7f3fa3ebeaf2ad2ac7c59013d96e56edefacac38
Chuks <chuksonyekams@gmail.com>
2022-09-19 06:37:37 -0700

    Debugging

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - to swap the values of intergers.
+ * @a: first interger pointer.
+ * @b: second interger pointer.
+ **/
+
+void swap_int(int *a, int *b)
+{
+*a = b;
+*b = a;
+}

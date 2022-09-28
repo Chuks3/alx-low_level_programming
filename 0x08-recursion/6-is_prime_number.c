b3665dc74859be69eef02db42f50419ad32d849f
Chuks <chuksonyekams@gmail.com>
2022-09-28 11:07:28 -0700

    Debugging

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,37 +1,31 @@
-     1	#include "main.h"
-     2	int is_divisible(int num, int div);
-     3	
-     4	/**
-     5	 * is_prime_number - Afunction that checks if a number is prime.
-     6	 * @n: an input integer
-     7	 * Return: 1 if n is prime or  0 in otherwise
-     8	 */
-     9	int is_prime_number(int n)
-    10	{
-    11		int div = 2;
-    12	
-    13		if (n <= 1)
-    14			return (0);
-    15	
-    16		if (n <= 3)
-    17			return (1);
-    18	
-    19		return (is_divisible(n, div));
-    20	}
-    21	
-    22	/**
-    23	 * is_divisible - check if num is divisible
-    24	 * @num: the number to be checked
-    25	 * @div: the result of division
-    26	 * Return: 1 if num is divisible or 0 if numis not divisible
-    27	 */
-    28	int is_divisible(int num, int div)
-    29	{
-    30		if (num % div == 0)
-    31			return (0);
-    32	
-    33		if (div == num / 2)
-    34			return (1);
-    35	
-    36		return (is_divisible(num, div + 1));
-    37	}
+ #include "main.h"
+
+/**
+ * is_prime_number - checks if the input integer is a prime number
+ * @n: integer
+ * Return: an int
+ **/
+
+int helper(int i, int n)
+{
+	if (n % i == 0 && n != i)
+		return (0);
+	if (n % i != 0 && i < n)
+		return (helper(i + 1, n));
+	return (1);
+}
+
+/**
+ * is_prime_number - is prime or not
+ * @n: integer to compare
+ * Return: boolean
+ **/
+
+int is_prime_number(int n)
+{
+	int i = 2;
+
+	if (n < 2)
+		return (0);
+	return (helper(i, n));
+  }

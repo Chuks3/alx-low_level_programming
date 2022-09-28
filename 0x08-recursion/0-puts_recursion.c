fe014ffa623589a6d220f659a0e896dba90b0250
Chuks <chuksonyekams@gmail.com>
2022-09-28 08:11:25 -0700

    Debugging

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,10 +7,11 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s)
 	{
 		_putchar(*s);
-		_puts_recursion(s + 1);
+		_puts_recursion(++s);
 	}
-	_putchar('\n');
+	else
+		_putchar('\n');
 }

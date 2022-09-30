45a229375d9387206cb5230fb610dad2d4bf8f4b
Chuks <chuksonyekams@gmail.com>
2022-09-30 16:48:31 -0700

    Create script

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _isdigit - checks for digits.
+* @c: the character it checks.
+* Return: Always 0.
+*/
+
+int _isdigit(int c)
+{
+return (c >= '0' && c <= '9');
+}
---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - check the code.
+ * @c: Is an interger.
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+return (c >= 'a' && c <= 'z');
+}

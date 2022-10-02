32c16df896c5b325d36cf09c04d161cf95a8af78
Chuks <chuksonyekams@gmail.com>
2022-10-02 13:38:09 -0700

    Debugging

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints arguments count
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: always 0
+ **/
+
+int main(int argc, __attribute__((unused)) char **argv)
+{
+	printf("%i\n", argc);
+	return (0);
+}

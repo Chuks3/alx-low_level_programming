67fc581fe4d91cc7c2809790f44526c3209a69c2
Chuks3 <chuksonyekams@gmail.com>
2022-10-07 02:06:30 +0100

    Create File

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,19 +12,24 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i, j;
-	char *a = malloc(*s1 + n);
+	char *a = malloc(*s1 + n + 1);
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
 
 	for (i = 0; s1[i]; i++)
-		a[i] = s1[i];
+		a[j++] = s1[i];
 
 	for (j = 0; j <= n; j++)
-	{
-		a[i] = s2[j];
-		i += 1;
-	}
+		a[j++] = s2[i];
 
 	if (a == NULL)
 		return (NULL);
+
+	a[j++] = '\0';
 	return (a);
 	free(a);
 }

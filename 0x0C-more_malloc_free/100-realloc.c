990bf997b200ad6f0a4b5d3fa8163b62992fe077
Chuks <chuksonyekams@gmail.com>
2022-10-06 20:12:53 -0700

    Debugging

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,7 +10,8 @@
  * Return: If new_size == old_size - ptr.
  *         If new_size == 0 and ptr is not NULL - NULL.
  *         Otherwise - a pointer to the reallocated memory block.
- */
+ **/
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *mem;

91f0eae05aa7be659b6d7c777f4a6bacb1443446
Chuks3 <chuksonyekams@gmail.com>
2022-10-07 03:57:58 +0100

    Debug

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,51 +1,34 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * *_memcpy - copies memory area
- *@dest: destination to copy
- *@src: source pointer
- *@n: number of bytes to coppy
- *Return: dest (destination)
- */
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-	{
-		dest[i] = src[i];
-	}
-	return (dest);
-}
-
-/**
- * *_realloc - reallocate a memory block using malloc and free
- *@ptr: ponter to realloc
- *@old_size: old size of the memory
- *@new_size: new size of the memory
- *Return: pointer reallocared or NULL
+ * _realloc - Reallocates a memory block using malloc and free.
+ * @ptr: A pointer to the memory previously allocated.
+ * @old_size: The size in bytes of the allocated space for ptr.
+ * @new_size: The size in bytes for the new memory block.
+ *
+ * Return: If new_size == old_size - ptr.
+ *         If new_size == 0 and ptr is not NULL - NULL.
+ *         Otherwise - a pointer to the reallocated memory block.
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *rp;
-	unsigned int i;
-
-	if (ptr == NULL)
-	{
-		rp = malloc(new_size);
-		if (rp == NULL)
-		{
-			free(ptr);
-			return (NULL);
-		}
-		free(ptr);
-		return (rp);
-	}
+	void *mem;
+	char *ptr_copy, *filler;
+	unsigned int index;
 
 	if (new_size == old_size)
 		return (ptr);
+
+	if (ptr == NULL)
+	{
+		mem = malloc(new_size);
+
+		if (mem == NULL)
+			return (NULL);
+
+		return (mem);
+	}
 
 	if (new_size == 0 && ptr != NULL)
 	{
@@ -53,17 +36,20 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 
-	rp = malloc(new_size);
+	ptr_copy = ptr;
+	mem = malloc(sizeof(*ptr_copy) * new_size);
 
-	if (rp == NULL)
+	if (mem == NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
 
-	if (new_size > old_size)
-		_memcpy(rp, ptr, old_size);
-	free(ptr);
+	filler = mem;
 
-	return (rp);
+	for (index = 0; index < old_size && index < new_size; index++)
+		filler[index] = *ptr_copy++;
+
+	free(ptr);
+	return (mem);
 }

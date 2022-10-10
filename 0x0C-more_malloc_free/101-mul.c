185344aeae42fc1b1e082a1681c4a00ab7bdf944
Chuks <chuksonyekams@gmail.com>
2022-10-10 08:56:38 -0700

    Debugging

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,32 +3,75 @@
 #include "main.h"
 
 /**
- * main - multiplies two positive numbers.
- * @argc: argument count
- * @argv: argument vector
- * Return: always zero otherwise 98
- **/
+ * main - multiplies two numbers
+ * @s: string to be converted
+ *
+ * Return: 0 on success,
+ * otherwise 1
+ */
+int _atoi(char *s);
 
 int main(int argc, char **argv)
 {
-	int num1, num2, num;
+	int num1, num2;
 
-	if (argc != 3)
+	if (!(argc == 3))
 	{
 		printf("Error\n");
-		return (98);
+		exit (98);
 	}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
+	num1 = _atoi(*(argv + 1));
+	num2 = _atoi(*(argv + 2));
 
-	if ((num1 > 9 && num1 < 100) && (num2 > 9 && num2 < 100))
+	printf("%d\n", num1 * num2);
+
+	return (0);
+}
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
 	{
-		num = num1 * num2;
-		printf("%d\n", num);
-		return (0);
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
 	}
 
-	printf("Error\n");
-	return (98);
+	if (f == 0)
+	{
+		printf("Eror\n");
+		exit (98);
+	}
+	return (n);
 }
